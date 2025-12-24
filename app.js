9127bf578f34bc643620e60bd364e19599186f7c
AvrahamTz <99908280+AvrahamTz@users.noreply.github.com>
2025-12-24 12:21:46 +0200

    Replace posts route with agents route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import usersRoutes from "./routes/usersR.js";
-import postsRoutes from "./routes/postsR.js";
+import postsRoutes from "./routes/agentsR.js";
 const app = express();
 const port = 3002;
 app.use(express.json());
@@ -8,11 +8,8 @@ app.use(express.json());
 // users routes
 app.use("/users", usersRoutes);
 // posts routes
-app.use("/posts", postsRoutes);
+app.use("/agents", postsRoutes);
 
-app.use((err, req, res, next) => {
-    res.status(500).json({ err: err ? err : "internal error" });
-})
 
 app.listen(port, () => {
     console.log(`server runing on http://localhost:${port}`);
